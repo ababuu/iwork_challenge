7520f1dde1da469ce135b20945df971d6c49b612
Ababu Alemu <63737795+ababuu@users.noreply.github.com>
2022-01-28 10:03:54 +0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,5 +1,5 @@
-const tel1 = '+251 (11) 650 7058';
-const tel2 = '+1 (424) 341-3346';
+const tel1 = document.evaluate('//*[@id="comp-kktfmgn8"]/p/span', document,null, XPathResult.ANY_TYPE,null).iterateNext().textContent;
+const tel2 = document.evaluate('//*[@id="comp-ks7htvty"]/p/span', document,null, XPathResult.ANY_TYPE,null).iterateNext().textContent;
 const officeNumber=Number('+251 (11) 650 7058'.slice(-4));
 const mobileNumber=Number('+1 (424) 341-3346'.slice(-4));
 const result=officeNumber/mobileNumber;
